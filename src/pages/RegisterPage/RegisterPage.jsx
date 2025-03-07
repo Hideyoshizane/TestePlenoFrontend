ba5e221cce5a9acb06bc9a5cc65fdffc7f2c8f13
Hideki Yoshizane <40189933+Hideyoshizane@users.noreply.github.com>
2025-03-07 19:40:39 -0300

    RegisterPage finished

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import { useNavigate } from 'react-router-dom';
+
+import Logo from '../../../src/assets/logo/logo.png';
+import styles from './RegisterPage.module.css';
+
+import RegisterForm from '../../Components/RegisterForm/RegisterForm';
+
 const RegisterPage = () => {
-	return <h1>About Page</h1>;
+	const navigate = useNavigate();
+
+	const handleClickRedirect = () => {
+		navigate('/');
+	};
+	return (
+		<div className={styles.container}>
+			<div className={styles.logoButton}>
+				<img src={Logo} alt="Capys Logo" />
+				<button className={styles.returnButton} onClick={handleClickRedirect}>
+					Voltar
+				</button>
+			</div>
+			<RegisterForm />
+		</div>
+	);
 };
 
 export default RegisterPage;
